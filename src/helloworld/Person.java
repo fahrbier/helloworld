eb5ed44cfb83659c56254b642444c907712ee280
Holger Lersch <Holger@Lersch.net>
2017-03-17 12:22:34 -0400

    added a person class

---
+++ src/helloworld/Person.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author holger
+ */
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+        
+}
