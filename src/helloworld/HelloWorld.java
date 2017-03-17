eecd10b860bf32dcd9f971c5f3381251f52a4a16
Holger Lersch <Holger@Lersch.net>
2017-03-17 12:22:48 -0400

    make use of person

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,8 +15,13 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         System.out.println("Hello World");
+        Person owner = new Person("Holger");
+        System.out.println("Hello " + owner.getName());
+        owner.setName("Peter");
+        System.out.println("Hello " + owner.getName());
+
     }
     
 }
